c7ca81516d5ae078bfeb2fe1b3a5d42855b7de2d
Amigo Prompt <125986188+amigoprompt@users.noreply.github.com>
2023-02-21 16:43:54 +0000

    answering a quiz

---
+++ day01/main.py
@@ -0,0 +1,6 @@
+#Write a program to prints a number of character from user input
+#Ans 
+# create a variable to take user input
+user_take_information = input("What is your Name? ")
+#now print the user name and count the characters we use len() function
+print(len(user_take_information))
